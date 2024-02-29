23fd52a43f279fd9067372a509dc3758afd5de6e
Mikhail Plyusnin <m.plyusnin@cloudaware.com>
2024-02-29 21:01:30 +0100

    add c# 9.0 features

---
+++ src/9.0/InitOnlySetters/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace InitOnlySetters;
+
+class MyClass
+{
+    private readonly string name;
+
+    public string Name
+    {
+        get => name;
+        init => name = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+    }
+
+    public int Age { get; init; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass obj = new MyClass
+        {
+            Name = "name",
+            Age = 12
+        };
+        
+        Console.WriteLine(obj);
+    }
+}
---
+++ src/9.0/RecordTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RecordTypes;
+
+record MySimpleRecord1(string Name, int Age);
+
+record MySimpleRecord2()
+{
+    private string Name { get; init; }
+    private int Age { get; init; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MySimpleRecord1 record1 = new MySimpleRecord1("petr", 11);
+        MySimpleRecord1 record2 = new MySimpleRecord1("petr", 11);
+
+        Console.WriteLine(record1 == record2);
+        Console.WriteLine(record1);
+
+        (string name, int age) = record1;
+        Console.WriteLine("{0}:{1}", name, age);
+
+        record1 = record1 with {Name = "vasya"};
+        Console.WriteLine(record1);
+    }
+}
